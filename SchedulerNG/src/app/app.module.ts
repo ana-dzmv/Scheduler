a5083babdaf1c6b2c2e20432c6d926830da64e4c
Anastasia <ana.dzmv@gmail.com>
2017-02-25 00:58:04 +0200

    DIR NG: basic inital components added

--- SchedulerNG/src/app/app.module.ts
+++ SchedulerNG/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MainSectionComponent } from './main-section/main-section.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SchedulerComponent } from './scheduler/scheduler.component';
+import { ToolboxComponent } from './toolbox/toolbox.component';
+import { NewTaskFormComponent } from './new-task-form/new-task-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainSectionComponent,
+    SidebarComponent,
+    SchedulerComponent,
+    ToolboxComponent,
+    NewTaskFormComponent
   ],
   imports: [
     BrowserModule,
