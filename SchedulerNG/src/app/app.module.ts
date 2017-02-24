0069b24cd48bad3784c389b05ad3a4e780e26aae
Anastasia <ana.dzmv@gmail.com>
2017-02-25 00:33:03 +0200

    DIR: rebuilding new ng project to renew component names

--- SchedulerNG/src/app/app.module.ts
+++ SchedulerNG/src/app/app.module.ts
@@ -4,22 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { NgAppComponent } from './ng-app/ng-app.component';
-import { NgMainSectionComponent } from './ng-main-section/ng-main-section.component';
-import { NgSidebarComponent } from './ng-sidebar/ng-sidebar.component';
-import { NgNewTaskFormComponent } from './ng-new-task-form/ng-new-task-form.component';
-import { NgScheduleComponent } from './ng-schedule/ng-schedule.component';
-import { NgSettingsDdComponent } from './ng-settings-dd/ng-settings-dd.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NgAppComponent,
-    NgMainSectionComponent,
-    NgSidebarComponent,
-    NgNewTaskFormComponent,
-    NgScheduleComponent,
-    NgSettingsDdComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
